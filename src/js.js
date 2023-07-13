e54cc29a4b34c070845243b89a7c3e570af5384c
Olga-Olga <olga_l@ukr.net>
2023-07-13 14:48:56 +0300

    1239

--- src/js.js
+++ src/js.js
@@ -85,7 +85,14 @@ async function loadMore() {
         return
     }
 
+const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
 
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
 
 
    }
